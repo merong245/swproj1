c23385babeb8dcb99dad3c3c97cfc4372e2d65f0
LeeJunHyeok <merong245@gmail.com>
2018-09-28 18:46:44 +0900

    wordNode
    implement wordnode

--- src/p1/WordNode.java
+++ src/p1/WordNode.java
@@ -10,47 +10,48 @@ public class WordNode {
 	
 	WordNode()
 	{
-		
+		wordmean.first=wordmean.second=null;
+		pLeft=pRight=pNext=null;
 	}
 
 	public String GetWord()
 	{
-
+		return wordmean.first;
 	}
 	public String GetMean()
 	{
-
+		return wordmean.second;
 	}
 	public WordNode GetLeft()
 	{
-
+		return pLeft; 
 	}
 	public WordNode GetRight()
 	{
-
+		return pRight;
 	}
 	public WordNode GetNext()
 	{
-
+		return pNext;
 	}
 	public void	SetWord(String word)
 	{
-
+		this.wordmean.first=wordmean.first;
 	}
 	public void SetMean(String mean)
 	{
-
+		this.wordmean.second=wordmean.second;
 	}
 	public void	SetLeft(WordNode node)
 	{
-	
+		pLeft=node.pLeft;
 	}
 	public void	SetRight(WordNode node)
 	{
-
+		pRight=node.pRight;
 	}
 	public void	SetNext(WordNode node)
 	{
-
+		pNext=node.pNext;
 	}	
 }
